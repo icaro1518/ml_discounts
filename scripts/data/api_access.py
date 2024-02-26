c0c0d4cf00e1599739c28b729d71b32d9d32291e
Heiler Santiago Gómez Prieto <hsgomezp@unal.edu.co>
2024-02-25 21:02:47 -0500

    Documenting api access and data acquisition classes

--- scripts/data/api_access.py
+++ scripts/data/api_access.py
@@ -1,21 +1,25 @@
+""" Api Access Module"""
+
 import requests
 from pathlib import Path
-from scripts.data.secrets.api_access_data import PATH_ACCESS_TOKEN
+from scripts.data.secrets.api_access_data import PATH_ACCESS_TOKEN, PATH_REFRESH_TOKEN
 
-def get_access_token(client_secret, app_id, code_user, redirect_uri):
+def get_access_token(client_secret: str, app_id: str, code_user: str, redirect_uri: str) -> dict:
     """Get access token from MercadoLibre API
-    Solo se requiere una promera vez para obtener el access token y el refresh token.
-    See: https://developers.mercadolibre.com.ar/es_ar/autenticacion-y-autorizacion/
+    It is only required once to obtain the access token and the refresh token, 
+    the secrets folder (scripts/data/secrets/access_token.txt) will be updated with the access token.
+    For a new access token, use the refresh_access_token method.
     
-
+    See: https://developers.mercadolibre.com.ar/es_ar/autenticacion-y-autorizacion/#Autorizaci%C3%B3n
+    
     Args:
-        client_secret (_type_): _description_
-        app_id (_type_): _description_
-        code_user (_type_): _description_
-        redirect_uri (_type_): _description_
+        client_secret (str): Client secret from the app.
+        app_id (str): App id from the app.
+        code_user (str): Code user generated after the user login.
+        redirect_uri (str): Redirect URI from the app.
 
     Returns:
-        _type_: _description_
+        response (dict): Response from the API.
     """
     url = "https://api.mercadolibre.com/oauth/token"
     payload = f'grant_type=authorization_code&client_id={app_id}&client_secret={client_secret}&code={code_user}&redirect_uri={redirect_uri}'
@@ -28,10 +32,26 @@ def get_access_token(client_secret, app_id, code_user, redirect_uri):
     response = response.json()
     with open(PATH_ACCESS_TOKEN, "w") as file:
         file.write(response["access_token"])
+        
+    with open(PATH_REFRESH_TOKEN, "w") as file:
+        file.write(response["refresh_token"])
 
     return response
 
-def refresh_access_token(client_secret, app_id, refresh_token):
+def refresh_access_token(client_secret: str, app_id: str, refresh_token: str) -> str:
+    """Generate a new access token from the refresh token and update the access_token file in
+    the secrets folder (scripts/data/secrets/access_token.txt).
+    
+    See: https://developers.mercadolibre.com.ar/es_ar/autenticacion-y-autorizacion/#Refresh-token
+
+    Args:
+        client_secret (str): Client secret from the app.
+        app_id (str): App id from the app.
+        refresh_token (str): Refresh token from the get_access_token method.
+
+    Returns:
+        response["access_token"] (str): String with the new access token.
+    """
     url = "https://api.mercadolibre.com/oauth/token"
     payload = f'grant_type=refresh_token&client_id={app_id}&client_secret={client_secret}&refresh_token={refresh_token}'
     headers = {
@@ -43,9 +63,21 @@ def refresh_access_token(client_secret, app_id, refresh_token):
     with open(PATH_ACCESS_TOKEN, "w") as file:
         file.write(response["access_token"])
 
-    return response
+    return response["access_token"]
 
-def call_access_token():
+def call_access_token()->str:
+    """Call access token string from the secrets folder (scripts/data/secrets/access_token.txt).
+    Returns:
+        str: Aceess token string.
+    """
     with open(PATH_ACCESS_TOKEN, "r") as file:
+        return file.read()
+    
+def call_refresh_token()->str:
+    """Call refresh token string from the secrets folder (scripts/data/secrets/refresh_token.txt).
+    Returns:
+        str: refresh token string.
+    """
+    with open(PATH_REFRESH_TOKEN, "r") as file:
         return file.read()
 
--- scripts/data/data_acquisition.py
+++ scripts/data/data_acquisition.py
@@ -1,3 +1,4 @@
+""" Data Acquisition classes. """
 import requests
 import pandas as pd
 import numpy as np 
@@ -7,50 +8,117 @@ from concurrent.futures import ThreadPoolExecutor
 from scripts.data.api_access import call_access_token
 
 class DataAcquisition:
+    """
+    This class is responsible for acquiring data from MercadoLibre API, its the base for items, 
+    sellers and ratings.
 
-    def __init__(self, path_data, country_site = "MCO") -> None:
+    Args:
+        path_data (Path): The location where the extracted data will be saved.
+        country_site (str, optional): The country identifier to extract data from. Defaults to 'MCO'.
+    """
+
+    def __init__(self, path_data: Path, country_site: str="MCO") -> None:
         """
-        NOTA: Máximo offset permitido por la API de MercadoLibre es 1000
+        Initializes a new instance of the DataAcquisition class.
 
         Args:
-            path_data (Path): Ubicación donde se guardará la data extraída.
-            country_site (str, optional): Identificador del país para extraer data. Defaults to 'MCO'.
+            path_data (Path): The location where the extracted data will be saved.
+            country_site (str, optional): The country identifier to extract data from. Defaults to 'MCO'.
         """
         self.path_data = path_data
         self.country_site = country_site
         self.cats = self.get_categories()
         self.access_token = call_access_token()
-        
-    def get_categories(self):
+
+    def get_categories(self) -> pd.DataFrame:
+        """
+        Retrieves the categories from the MercadoLibre API.
+
+        Returns:
+            pandas.DataFrame: The categories data.
+        """
         cats = requests.get(f'https://api.mercadolibre.com/sites/{self.country_site}/categories')
         cats = pd.DataFrame(cats.json())
-        cats = cats.sort_values(by = "id").reset_index(drop = True)
+        cats = cats.sort_values(by="id").reset_index(drop=True)
         return cats
-    
+
     @staticmethod
-    def save_data(data, path, preffix, filename = ""):
+    def save_data(data: pd.DataFrame, path: Path, preffix: str, filename: str="") -> None:
+        """
+        Saves the data to a CSV file.
+
+        Args:
+            data (pandas.DataFrame): The data to be saved.
+            path (Path): The location where the data will be saved.
+            preffix (str): The prefix for the filename.
+            filename (str, optional): The filename after the preffix. Defaults to "".
+        """
         data.to_csv(path / f'{preffix}_{filename}.csv', index=False)
-    
+
     @staticmethod
-    def compile_data(preffix, path):
+    def compile_data(preffix: str, path: Path) -> pd.DataFrame:
+        """
+        Compiles the data from multiple CSV files into a single DataFrame.
+
+        Args:
+            preffix (str): The prefix of the CSV files to be compiled.
+            path (Path): The location of the CSV files.
+
+        Returns:
+            pandas.DataFrame: The compiled data.
+        """
         files = list(path.glob(f'{preffix}*.csv'))
         data = pd.concat([pd.read_csv(file) for file in files], ignore_index=True)
         return data
     
 class ItemDataAcquisition(DataAcquisition):
-    
-    def __init__(self, path_data, country_site = "MCO") -> None:
+    """
+    This class represents the data acquisition for items.
+    It inherits from the DataAcquisition class.
+    """
+
+    def __init__(self, path_data, country_site="MCO") -> None:
+        """
+        Initializes an instance of the ItemDataAcquisition class.
+
+        Parameters:
+        - path_data (str): The path to the data.
+        - country_site (str): The country site. Default is "MCO".
+        """
         super().__init__(path_data, country_site)
     
     @staticmethod
-    def get_only_one_attr(row, attribute):
+    def get_only_one_attr(row: pd.Series, attribute: str) -> None:
+        """
+        Retrieves the attribute from a given row.
+
+        Parameters:
+        - row (pd.Series): The row containing attributes.
+        - attribute (str): The attribute to retrieve.
+
+        Returns:
+        - dict: The attribute dictionary if found, None otherwise.
+        """
         for i in range(len(row)):
             if row[i]["id"] == attribute:
                 return row[i]
-        return None  
+        return None
     
-    def items_by_category(self, category, offset):
+    def items_by_category(self, category: str, offset: int) -> pd.DataFrame:
+        """
+        Retrieves items by category and offset.
+        Max offset for public API: 1000
+        Max offset for registered APP in API: 4000
         
+        See: https://developers.mercadolibre.com.ar/es_ar/items-y-busquedas
+
+        Parameters:
+        - category (str): The category of the items.
+        - offset (int): The offset for pagination.
+
+        Returns:
+        - pandas.DataFrame: The items data.
+        """
         url = f'https://api.mercadolibre.com/sites/{self.country_site}/search?category={category}&offset={offset}'       
         headers = {
             'Authorization': f'Bearer {self.access_token}'
@@ -74,8 +142,14 @@ class ItemDataAcquisition(DataAcquisition):
         
         return items
     
-    def get_all_items_by_cats(self, init_offset, final_offset):
-        
+    def get_all_items_by_cats(self, init_offset: int, final_offset: int) -> None:
+        """
+        Retrieves all items by categories within the specified offset range.
+
+        Parameters:
+        - init_offset (int): The initial offset.
+        - final_offset (int): The final offset.
+        """
         offset_range = np.arange(init_offset, final_offset+50, 50)
         for category, offset in product(self.cats['id'], offset_range):
             cat_data = self.items_by_category(category, offset)
@@ -84,7 +158,16 @@ class ItemDataAcquisition(DataAcquisition):
             # Saving extracted data
             self.save_data(cat_data, self.path_data, "data_items", f"{category}_{offset}")
     
-    def explode_data(self, df_items):
+    def explode_data(self, df_items: pd.DataFrame) -> pd.DataFrame:
+        """
+        Explodes the data in the specified DataFrame.
+
+        Parameters:
+        - df_items (pandas.DataFrame): The DataFrame containing the items data.
+
+        Returns:
+        - pandas.DataFrame: The exploded data.
+        """
         cols_explode = ['shipping', 'seller', 'installments', 'brand']
         df_explode_final = pd.DataFrame()
         for col in cols_explode:
@@ -96,7 +179,16 @@ class ItemDataAcquisition(DataAcquisition):
         return df_explode_final
     
     @staticmethod
-    def clean_data_cols(df):
+    def clean_data_cols(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Cleans the specified DataFrame columns.
+
+        Parameters:
+        - df (pandas.DataFrame): The DataFrame to clean.
+
+        Returns:
+        - pandas.DataFrame: The cleaned DataFrame.
+        """
         cols_delete = ['thumbnail_id', 'thumbnail', 'currency_id', 'order_backend',
                        'use_thumbnail_id', 'attributes', 'installments', 
                        'differential_pricing', 'inventory_id', 'variation_filters',
@@ -108,102 +200,153 @@ class ItemDataAcquisition(DataAcquisition):
         return df.drop(columns = existing_columns)
 
 class SellerDataAcquisition(DataAcquisition):
-        
-        def __init__(self, path_data, country_site = "MCO", MAX_THREADS = 6) -> None:
-            super().__init__(path_data, country_site)
-            self.MAX_THREADS = MAX_THREADS
-                               
-        def get_seller(self, seller_id: str):
-            url = f'https://api.mercadolibre.com/users/{seller_id}'       
-            headers = {
-                'Authorization': f'Bearer {self.access_token}'
-                }
-            payload = {}
-            seller_data = requests.request("GET", url, headers=headers, data=payload)
-            seller_data = seller_data.json()
-            
-            usertype_data = seller_data["user_type"]
-            final_data = self.get_seller_reputation_data(seller_data["seller_reputation"])
-            
-            final_data["seller_id"] = seller_id
-            final_data["user_type"] = usertype_data
-            
-            return final_data
-        
-        def get_seller_reputation_data(self, rep_data):
-        
-            # Get values from transactions
-            rep_data['transactions_period'] = rep_data['transactions']['period']
-            rep_data['transactions_total'] = rep_data['transactions']['total']
+    """
+    This class represents the data acquisition for sellers.
+    It inherits from the DataAcquisition class.
+    """
 
-            # Delete data about transactions
-            del rep_data['transactions']
+    def __init__(self, path_data, country_site="MCO", MAX_THREADS=6) -> None:
+        """
+        Initializes a new instance of the SellerDataAcquisition class.
 
-            # Create dataframe from dictionary
-            final_data = pd.DataFrame([rep_data])
-            
-            return final_data
-        
-        def get_all_sellers(self, sellers_id: list):
-        
-            sellers_data = pd.DataFrame()
-            with ThreadPoolExecutor(max_workers=self.MAX_THREADS) as executor:
-                sellers_data = executor.map(self.get_seller, sellers_id)
-            sellers_data = pd.concat(list(sellers_data))
-            
-            self.save_data(sellers_data, self.path_data, "seller_data")
-            
-            return sellers_data
+        Parameters:
+        - path_data (str): The path to the data.
+        - country_site (str): The country site. Default is "MCO".
+        - MAX_THREADS (int): The maximum number of threads. Default is 6.
+        """
+        super().__init__(path_data, country_site)
+        self.MAX_THREADS = MAX_THREADS
+
+    def get_seller(self, seller_id: str) -> pd.DataFrame:
+        """
+        Retrieves data for a specific seller.
+
+        Parameters:
+        - seller_id (str): The ID of the seller.
+
+        Returns:
+        - final_data (pd.DataFrame): The seller data.
+        """
+        url = f'https://api.mercadolibre.com/users/{seller_id}'
+        headers = {
+            'Authorization': f'Bearer {self.access_token}'
+        }
+        payload = {}
+        seller_data = requests.request("GET", url, headers=headers, data=payload)
+        seller_data = seller_data.json()
+
+        usertype_data = seller_data["user_type"]
+        final_data = self.get_seller_reputation_data(seller_data["seller_reputation"])
+
+        final_data["seller_id"] = seller_id
+        final_data["user_type"] = usertype_data
+
+        return final_data
+
+    def get_seller_reputation_data(self, rep_data: dict) -> pd.DataFrame:
+        """
+        Retrieves the reputation data for a seller.
+
+        Parameters:
+        - rep_data (dict): The reputation data.
+
+        Returns:
+        - final_data (pd.DataFrame): The reputation data as a DataFrame.
+        """
+        # Get values from transactions
+        rep_data['transactions_period'] = rep_data['transactions']['period']
+        rep_data['transactions_total'] = rep_data['transactions']['total']
+
+        # Delete data about transactions
+        del rep_data['transactions']
+
+        # Create dataframe from dictionary
+        final_data = pd.DataFrame([rep_data])
+
+        return final_data
+
+    def get_all_sellers(self, sellers_id: list)-> pd.DataFrame:
+        """
+        Retrieves data for multiple sellers.
+
+        Parameters:
+        - sellers_id (list): The list of seller IDs.
+
+        Returns:
+        - sellers_data (pd.DataFrame): The data for all sellers.
+        """
+        sellers_data = pd.DataFrame()
+        with ThreadPoolExecutor(max_workers=self.MAX_THREADS) as executor:
+            sellers_data = executor.map(self.get_seller, sellers_id)
+        sellers_data = pd.concat(list(sellers_data))
+
+        self.save_data(sellers_data, self.path_data, "seller_data")
+
+        return sellers_data
         
 class RatingsDataAcquisition(DataAcquisition):
-        
-        def __init__(self, path_data, country_site = "MCO", MAX_THREADS = 6) -> None:
-            super().__init__(path_data, country_site)
-            self.MAX_THREADS = MAX_THREADS
-        
-        def get_all_ratings(self, items_id: list):
-            
-            ratings_data = pd.DataFrame()            
-            with ThreadPoolExecutor(max_workers=self.MAX_THREADS) as executor:
-                ratings_data = executor.map(self.get_rating, items_id)
-            ratings_data = pd.concat(list(ratings_data))
-            self.save_data(ratings_data, self.path_data, "ratings_data")
-            
-            return ratings_data
-                            
-        def get_rating(self, item_id: str):
-            try:
-                url = f'https://api.mercadolibre.com/reviews/item/{item_id}'       
-                headers = {
-                    'Authorization': f'Bearer {self.access_token}'
-                    }
-                payload = {}
-                rating_data = requests.request("GET", url, headers=headers, data=payload)
-                rating_data = rating_data.json()
-                final_data = pd.DataFrame([rating_data["rating_levels"]])
-                final_data["total_reviews"] = final_data.sum(axis = 1)
-                final_data["rating_average"] = rating_data["rating_average"]
-                
-                final_data["id"] = item_id
-                self.save_data(final_data, self.path_data, "ratings_data", item_id)
-                
-                return final_data
-            except Exception as e:
-                print('Request failed due to error:', e)
-                print("|"+item_id+"|")
-                return pd.DataFrame()
-            
-            # url = f'https://api.mercadolibre.com/reviews/item/{item_id}'       
-            # headers = {
-            #     'Authorization': f'Bearer {self.access_token}'
-            #     }
-            # payload = {}
-            # rating_data = requests.request("GET", url, headers=headers, data=payload)
-            # rating_data = rating_data.json()
-            # final_data = pd.DataFrame([rating_data["rating_levels"]])
-            # final_data["total_reviews"] = final_data.sum(axis = 1)
-            # final_data["rating_average"] = rating_data["rating_average"]
-            
-            # final_data["id"] = item_id
-            
-            # return final_data
+    """
+    This class represents the data acquisition for ratings data.
+    It inherits from the DataAcquisition class.
+    """
+
+    def __init__(self, path_data, country_site="MCO", MAX_THREADS=6) -> None:
+        """
+        Initializes a RatingsDataAcquisition object.
+
+        Parameters:
+        - path_data (str): The path to the data.
+        - country_site (str): The country site (default is "MCO").
+        - MAX_THREADS (int): The maximum number of threads to use (default is 6).
+        """
+        super().__init__(path_data, country_site)
+        self.MAX_THREADS = MAX_THREADS
+
+    def get_all_ratings(self, items_id: list) -> pd.DataFrame:
+        """
+        Retrieves all ratings data for the given list of item IDs.
+
+        Parameters:
+        - items_id (list): The list of item IDs.
+
+        Returns:
+        - ratings_data (DataFrame): The ratings data as a pandas DataFrame.
+        """
+        ratings_data = pd.DataFrame()
+        with ThreadPoolExecutor(max_workers=self.MAX_THREADS) as executor:
+            ratings_data = executor.map(self.get_rating, items_id)
+        ratings_data = pd.concat(list(ratings_data))
+        self.save_data(ratings_data, self.path_data, "ratings_data")
+
+        return ratings_data
+
+    def get_rating(self, item_id: str) -> pd.DataFrame:
+        """
+        Retrieves the rating data for a specific item ID.
+
+        Parameters:
+        - item_id (str): The item ID.
+
+        Returns:
+        - final_data (DataFrame): The rating data as a pandas DataFrame.
+        """
+        try:
+            url = f'https://api.mercadolibre.com/reviews/item/{item_id}'
+            headers = {
+                'Authorization': f'Bearer {self.access_token}'
+            }
+            payload = {}
+            rating_data = requests.request("GET", url, headers=headers, data=payload)
+            rating_data = rating_data.json()
+            final_data = pd.DataFrame([rating_data["rating_levels"]])
+            final_data["total_reviews"] = final_data.sum(axis=1)
+            final_data["rating_average"] = rating_data["rating_average"]
+
+            final_data["id"] = item_id
+            self.save_data(final_data, self.path_data, "ratings_data", item_id)
+
+            return final_data
+        except Exception as e:
+            print('Request failed due to error:', e)
+            print("|" + item_id + "|")
+            return pd.DataFrame()
