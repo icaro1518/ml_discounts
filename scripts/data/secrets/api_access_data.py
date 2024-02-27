8f97acf740d3f4d9265bb7b406dd199f3c2abce9
Heiler Santiago Gómez Prieto <hsgomezp@unal.edu.co>
2024-02-26 23:52:50 -0500

    Add secrets for API access

---
+++ scripts/data/secrets/api_access_data.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+PATH_ACCESS_TOKEN = Path(r"LLENAR_CARPETA_ACCESS_TOKEN\scripts\data\secrets\access_token.txt")
+PATH_REFRESH_TOKEN = Path(r"LLENAR_CARPETA_REFRESH_TOKEN\scripts\data\secrets\refresh_token.txt")
+client_secret = "CLIENT_SECRET"
+app_id = "CODIGO_APP_ID"
+redirect_uri = "REDIRECT_URI"
+code_user = "CODE_USER"
